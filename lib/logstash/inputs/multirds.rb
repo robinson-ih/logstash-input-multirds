5dcadedd0ed814bdfea1d598ed1b07ad0cc8a437
Robert Labrie <robert.labrie@gmail.com>
2018-11-21 12:47:36 -0500

    +ensure_lock_table

--- lib/logstash/inputs/multirds.rb
+++ lib/logstash/inputs/multirds.rb
@@ -20,7 +20,50 @@ class LogStash::Inputs::Multirds < LogStash::Inputs::Base
   config :log_file_name_pattern, :validate => :string, :required => true
   config :polling_frequency, :validate => :number, :default => 600
   config :group_name, :validate => :string, :required => true
-  
+
+  def ensure_lock_table(db, table)
+    begin
+        tables = db.list_tables({
+
+        })
+        return true if tables.to_h[:table_names].to_a.include?(table)
+        # TODO: there is a potential race condition here where a table could come back in list_tables but not be in ACTIVE state we should check this better
+        result = db.create_table({
+            table_name: table,
+            key_schema: [
+                {
+                    attribute_name: 'id',
+                    key_type: 'HASH'
+                }
+            ],
+            attribute_definitions: [
+                {
+                    attribute_name: 'id',
+                    attribute_type: 'S'
+                }
+            ],
+            provisioned_throughput: {
+                read_capacity_units: 10,
+                write_capacity_units: 10
+            }
+        })
+        puts "#{result.to_h}"
+        # wait here for the table to be ready
+        (1..10).each do |i|
+            sleep i
+            rsp = db.describe_table({
+                table_name: table
+            })
+            puts "#{rsp.to_h}"
+            return true if rsp.to_h[:table][:table_status] == 'ACTIVE'
+        end
+    rescue => e
+        puts "EXCEPTION #{e}"
+        return false
+    end
+    return false
+  end
+
   def register
     # @logger.info "Registering multi-RDS input", :region => @region, :instance => @instance_name, :log_file => @log_file_name
     # @database = Aws::RDS::DBInstance.new @instance_name, aws_options_hash
@@ -30,46 +73,54 @@ class LogStash::Inputs::Multirds < LogStash::Inputs::Base
     @logger.info "aws_options_hash #{aws_options_hash}"
     @db = Aws::DynamoDB::Client.new aws_options_hash
     @rds = Aws::RDS::Client.new aws_options_hash
-    # TODO: Auto-create dynamodb table here -- should that be a param? 
+    @ready = ensure_lock_table @db, @group_name
   end
 
   def run(queue)
+    if !@ready
+      @logger.warn "multi-rds dynamodb lock table not ready, unable to proceed"
+      return false
+    end
     @thread = Thread.current
     Stud.interval(@polling_frequency) do
-      @logger.debug "finding #{@log_file_name} for #{@instance_name} starting #{@sincedb.read} (#{@sincedb.read.to_i * 1000})"
-      begin
-        logfiles = @database.log_files({
-          filename_contains: @log_file_name,
-          file_last_written: @sincedb.read.to_i * 1000,
-        })
-        logfiles.each do |logfile|
-          @logger.debug "downloading #{logfile.name} for #{@instance_name}"
-          more = true
-          marker = "0"
-          while more do
-            response = logfile.download({marker: marker})
-            response[:log_file_data].lines.each do |line|
-              @codec.decode(line) do |event|
-                decorate event
-                event.set "rds_instance", @instance_name
-                event.set "log_file", @log_file_name
-                queue << event
-              end
-            end
-            more = response[:additional_data_pending]
-            marker = response[:marker]
-          end
-          @sincedb.write (filename2datetime logfile.name)
-        end
-      rescue Aws::RDS::Errors::ServiceError
-        # the next iteration will resume at the same location
-        @logger.warn "caught AWS service error"
-      end
+
     end
+    # @thread = Thread.current
+    # Stud.interval(@polling_frequency) do
+    #   @logger.debug "finding #{@log_file_name} for #{@instance_name} starting #{@sincedb.read} (#{@sincedb.read.to_i * 1000})"
+    #   begin
+    #     logfiles = @database.log_files({
+    #       filename_contains: @log_file_name,
+    #       file_last_written: @sincedb.read.to_i * 1000,
+    #     })
+    #     logfiles.each do |logfile|
+    #       @logger.debug "downloading #{logfile.name} for #{@instance_name}"
+    #       more = true
+    #       marker = "0"
+    #       while more do
+    #         response = logfile.download({marker: marker})
+    #         response[:log_file_data].lines.each do |line|
+    #           @codec.decode(line) do |event|
+    #             decorate event
+    #             event.set "rds_instance", @instance_name
+    #             event.set "log_file", @log_file_name
+    #             queue << event
+    #           end
+    #         end
+    #         more = response[:additional_data_pending]
+    #         marker = response[:marker]
+    #       end
+    #       @sincedb.write (filename2datetime logfile.name)
+    #     end
+    #   rescue Aws::RDS::Errors::ServiceError
+    #     # the next iteration will resume at the same location
+    #     @logger.warn "caught AWS service error"
+    #   end
+    # end
   end
 
   def stop
-    Stud.stop! @thread
+    if @ready Stud.stop! @thread
   end
 
   def filename2datetime(name)
